99dd2c9869e99113c92e5af1237c31f0ae192682
GABBETA SATHWIKA <sathwika.gabbeta@publicissapient.com>
2024-09-17 14:16:02 +0530

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 3999;
 
 mongoose.connect('mongodb://mongo:27017/outpatients', {
     useNewUrlParser: true,
